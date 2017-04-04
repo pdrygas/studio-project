51914d8216224ccaba262d235089941116802b8d
Bloomin117 <bloomin@o2.pl>
2017-04-04 22:22:53 +0200

    Add shitty server from example, because nothing was fucking working, fuck this shit

---
+++ server/src/main/java/pl/edu/agh/ApplicationSecurity.java
@@ -0,0 +1,39 @@
+package pl.edu.agh;
+
+import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeaderBindingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private RESTAuthenticationEntryPoint authenticationEntryPoint;
+	@Autowired
+	private RESTAuthenticationFailureHandler authenticationFailureHandler;
+	@Autowired
+	private RESTAuthenticationSuccessHandler authenticationSuccessHandler;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+		builder.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin")
+				.password("admin").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/rest/**").authenticated();
+		http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+		http.formLogin().successHandler(authenticationSuccessHandler);
+		http.formLogin().failureHandler(authenticationFailureHandler);
+		http.logout().logoutSuccessUrl("/");
+
+		// CSRF tokens handling
+		http.addFilterAfter(new CsrfTokenResponseHeaderBindingFilter(), CsrfFilter.class);
+	}
+}
