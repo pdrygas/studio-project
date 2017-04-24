2e2e17712ba8ef9059c6770fa156dc9af6c04a61
Bloomin117 <bloomin@o2.pl>
2017-04-25 00:23:23 +0200

    Add basic tests to UserRepository

--- server/src/main/java/pl/edu/agh/Application.java
+++ server/src/main/java/pl/edu/agh/Application.java
@@ -1,85 +1,11 @@
 package pl.edu.agh;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.edu.agh.model.Resource;
-import pl.edu.agh.model.User;
-import pl.edu.agh.repository.ResourceRepository;
-import pl.edu.agh.repository.UserRepository;
-
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public InitializingBean insertDefaultDatabaseRows() {
-        return new InitializingBean() {
-            @Autowired
-            private UserRepository userRepo;
-            @Autowired
-            private ResourceRepository resourceRepo;
-
-            @Override
-            public void afterPropertiesSet() throws Exception {
-                User user = new User();
-                user.setUsername("user");
-                user.setPassword(hashPassword("user"));
-                user.setToken("secret_token");
-
-                try {
-                    resourceRepo.deleteAll();
-                    userRepo.deleteAll();
-
-                    userRepo.save(user);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-
-                Resource resource = new Resource();
-                resource.setUser(user);
-                resource.setContent("jakis kontekt");
-                resource.setTitle("Tytul");
-                insertResource(resource);
-
-                resource = new Resource();
-                resource.setUser(user);
-                resource.setContent("asdasdas fff gggt");
-                insertResource(resource);
-            }
-
-            private void insertResource(Resource resource) {
-                try {
-                    resourceRepo.save(resource);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            private String hashPassword(String password) {
-                try {
-                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                    byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-
-                    StringBuffer sb = new StringBuffer();
-                    for (int i = 0; i < hash.length; i++) {
-                        sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
-                    }
-
-                    return sb.toString();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                }
-                return "";
-            }
-        };
     }
 }
---
+++ server/src/main/resources/data-test.sql
@@ -0,0 +1,2 @@
+DELETE FROM users;
+INSERT INTO users(id, username, password, token) VALUES (0, 'user', 'user', 'secret_token');
