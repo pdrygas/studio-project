babb715c1fc84d6cf58222b0fd9ee578645f02f7
Bloomin117 <bloomin@o2.pl>
2017-04-26 00:40:59 +0200

    Add /resources tests

--- server/src/test/java/pl/edu/agh/controller/RestApiControllerTests.java
+++ server/src/test/java/pl/edu/agh/controller/RestApiControllerTests.java
@@ -1,5 +1,6 @@
 package pl.edu.agh.controller;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,6 +14,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import pl.edu.agh.Application;
 
 @RunWith(SpringRunner.class)
@@ -22,28 +25,62 @@ import pl.edu.agh.Application;
 public class RestApiControllerTests {
     @Autowired
     private TestRestTemplate restTemplate;
-    private final String AUTH_HEADER_NAME = "X-AUTH-TOKEN";
-    private final String GOOD_TOKEN = "secret_token";
+    private final String USER_TOKEN = "secret_token";
+    private final String USER2_TOKEN = "other_token";
+    private final String USER3_TOKEN = "other2_token";
 
     @Test
     public void missingTokenCauses500Error() throws Exception {
-        JSONObject result = new JSONObject(restTemplate.postForEntity("/test", null, String.class).getBody());
+        JSONObject result = new JSONObject(request("/test", HttpMethod.POST, null));
         Assert.assertEquals(500, result.get("status"));
     }
 
     @Test
     public void wrongTokenCauses500Error() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(AUTH_HEADER_NAME, GOOD_TOKEN + "some_text");
-        JSONObject result = new JSONObject(restTemplate.exchange("/test", HttpMethod.POST, new HttpEntity<>(null, headers), String.class).getBody());
+        JSONObject result = new JSONObject(request("/test", HttpMethod.POST, requestEntity(null, USER_TOKEN + "wrong")));
         Assert.assertEquals(500, result.get("status"));
     }
 
     @Test
     public void goodToken() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(AUTH_HEADER_NAME, GOOD_TOKEN);
-        String result = restTemplate.exchange("/test", HttpMethod.POST, new HttpEntity<>(null, headers), String.class).getBody();
+        String result = request("/test", HttpMethod.POST, requestEntity(null, USER_TOKEN));
         Assert.assertEquals("workin biatch!", result);
+    }
+
+    @Test
+    public void defaultUserHas2Resources() throws Exception {
+        JSONArray result = new JSONArray(request("/resources", HttpMethod.GET, requestEntity(null, USER_TOKEN)));
+        Assert.assertEquals(2, result.length());
+    }
+
+    @Test
+    public void otherUserHas1Resource() throws Exception {
+        JSONArray result = new JSONArray(request("/resources", HttpMethod.GET, requestEntity(null, USER2_TOKEN)));
+        Assert.assertEquals(1, result.length());
+    }
+
+    @Test
+    public void addNewResource() throws Exception {
+        JSONObject result = new JSONObject(request("/resources", HttpMethod.POST,
+                                           requestEntity(resourceParams("some title", "some content"), USER3_TOKEN)));
+        Assert.assertEquals("ok", result.get("result"));
+    }
+
+    private String request(String url, HttpMethod method, HttpEntity entity) {
+        return restTemplate.exchange(url, method, entity, String.class).getBody();
+    }
+
+    private MultiValueMap<String, String> resourceParams(String title, String content) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("title", title);
+        params.add("content", content);
+        return params;
+    }
+
+    private HttpEntity requestEntity(MultiValueMap<String, String> params, String token) {
+        final String AUTH_HEADER_NAME = "X-AUTH-TOKEN";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(AUTH_HEADER_NAME, token);
+        return new HttpEntity<>(params, headers);
     }
 }
--- server/src/test/resources/import.sql
+++ server/src/test/resources/import.sql
@@ -1,2 +1,9 @@
-DELETE FROM users;
-INSERT INTO users(id, username, password, token) VALUES (0, 'user', 'user', 'secret_token');
+INSERT INTO users(id, username, password, token)
+VALUES (0, 'user', 'user', 'secret_token'),
+       (1, 'other', 'other', 'other_token'),
+       (2, 'other2', 'other', 'other2_token');
+
+INSERT INTO resources(title, content, user_id)
+VALUES ('some title', 'lorem ipsum....', 0),
+       (null, 'some text', 0),
+       ('some resource', 'dsadadasdsss a sda d ad dsa ds', 1);
